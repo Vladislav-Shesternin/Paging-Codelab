84590c0f0719f2e1c299d64c702ecd6f014a6733
Vlad Shesternin <valdon1202@gmail.com>
2021-03-30 13:55:35 +0300

    Beginning of Work with Paging library.

--- app/src/main/java/com/example/android/codelabs/paging/Injection.kt
+++ app/src/main/java/com/example/android/codelabs/paging/Injection.kt
@@ -21,25 +21,12 @@ import com.example.android.codelabs.paging.api.GithubService
 import com.example.android.codelabs.paging.data.GithubRepository
 import com.example.android.codelabs.paging.ui.ViewModelFactory
 
-/**
- * Class that handles object creation.
- * Like this, objects can be passed as parameters in the constructors and then replaced for
- * testing, where needed.
- */
 object Injection {
 
-    /**
-     * Creates an instance of [GithubRepository] based on the [GithubService] and a
-     * [GithubLocalCache]
-     */
     private fun provideGithubRepository(): GithubRepository {
         return GithubRepository(GithubService.create())
     }
 
-    /**
-     * Provides the [ViewModelProvider.Factory] that is then used to get a reference to
-     * [ViewModel] objects.
-     */
     fun provideViewModelFactory(): ViewModelProvider.Factory {
         return ViewModelFactory(provideGithubRepository())
     }
--- app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubRepository.kt
@@ -26,31 +26,18 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import retrofit2.HttpException
 import java.io.IOException
 
-// GitHub page API is 1 based: https://developer.github.com/v3/#pagination
 private const val GITHUB_STARTING_PAGE_INDEX = 1
 
-/**
- * Repository class that works with local and remote data sources.
- */
 class GithubRepository(private val service: GithubService) {
 
-    // keep the list of all results received
     private val inMemoryCache = mutableListOf<Repo>()
 
-    // shared flow of results, which allows us to broadcast updates so
-    // the subscriber will have the latest data
     private val searchResults = MutableSharedFlow<RepoSearchResult>(replay = 1)
 
-    // keep the last requested page. When the request is successful, increment the page number.
     private var lastRequestedPage = GITHUB_STARTING_PAGE_INDEX
 
-    // avoid triggering multiple requests in the same time
     private var isRequestInProgress = false
 
-    /**
-     * Search repositories whose names match the query, exposed as a stream of data that will emit
-     * every time we get more data from the network.
-     */
     suspend fun getSearchResultStream(query: String): Flow<RepoSearchResult> {
         Log.d("GithubRepository", "New query: $query")
         lastRequestedPage = 1
@@ -96,11 +83,9 @@ class GithubRepository(private val service: GithubService) {
     }
 
     private fun reposByName(query: String): List<Repo> {
-        // from the in memory cache select only the repos whose name or description matches
-        // the query. Then order the results.
         return inMemoryCache.filter {
             it.name.contains(query, true) ||
-                (it.description != null && it.description.contains(query, true))
+                    (it.description != null && it.description.contains(query, true))
         }.sortedWith(compareByDescending<Repo> { it.stars }.thenBy { it.name })
     }
 
